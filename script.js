65ef931a75bf61a335ff353f06472b84dc77b60c
teriTheDesigner <tere1109@stud.kea.dk>
2024-04-12 18:22:10 +0200

    disable categories

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   start();
 });
 
-let categorizedQuestions = {};
+let allQuestions = {};
 let displayedCards = [];
-let currentCardIndex = null;
+let selectedCategory;
+let currentQuestion = {};
 
 async function start() {
   registerButtons();
@@ -23,6 +24,7 @@ function registerButtons() {
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
       const category = this.getAttribute("data-category");
+      selectedCategory = category;
       DisplayCard(category);
       document.querySelector(".overlay").style.display = "none";
     });
@@ -33,7 +35,7 @@ async function getQuestions() {
   try {
     const response = await fetch("questions.json");
     const data = await response.json();
-    categorizedQuestions = data.categories.reduce((acc, category) => {
+    allQuestions = data.categories.reduce((acc, category) => {
       acc[category.name] = category.questions;
       return acc;
     }, {});
@@ -51,58 +53,108 @@ function DisplayCard(category) {
   const container = document.querySelector(".container");
   container.classList.remove("zoom-in");
   container.classList.add("zoom-out");
-  const buttons = document.querySelectorAll(".button");
-  buttons.forEach((button) => {
-    button.classList.remove("hidden");
-    button.classList.add("display");
-  });
+  updateCardContents(category);
+  updateButtons();
+  animateContainer();
+}
 
+function updateCardContents(category) {
   setTimeout(() => {
-    const card = document.querySelector(".card");
-    const front = card.querySelector(".front");
-    const back = card.querySelector(".back");
-
-    let selectedCategory;
     if (category === "random") {
-      const categoryNames = Object.keys(categorizedQuestions);
-      selectedCategory =
-        categoryNames[Math.floor(Math.random() * categoryNames.length)];
+      handleRandomCategory();
     } else {
-      selectedCategory = category;
-    }
-
-    const categoryQuestions = categorizedQuestions[selectedCategory];
-    const randomIndex = Math.floor(Math.random() * categoryQuestions.length);
-    const randomQuestion = categoryQuestions[randomIndex];
-
-    front.innerText = randomQuestion.question;
-    back.innerText = randomQuestion.answer;
-
-    card.classList.remove(
-      "category1",
-      "category2",
-      "category3",
-      "category4",
-      "category5"
-    );
-
-    card.classList.add("category" + selectedCategory);
-
-    displayedCards.push({
-      category: selectedCategory,
-      questionIndex: randomIndex,
-    });
-
-    currentCardIndex = displayedCards.length - 1;
-
-    // Check if there is a previous card available
-    if (currentCardIndex === 0) {
-      document.querySelector(".button.previous").classList.add("hidden");
-    } else {
-      document.querySelector(".button.previous").classList.remove("hidden");
+      handleSpecificCategory(category);
     }
   }, 300);
+}
 
+function handleRandomCategory() {
+  const categoryNames = Object.keys(allQuestions);
+  const nonEmptyCategories = categoryNames.filter(
+    (cat) => allQuestions[cat].length > 0
+  );
+
+  if (nonEmptyCategories.length === 0) {
+    disableAllCategoryButtons();
+    disableRandomCategoryButton();
+    return;
+  }
+
+  selectedCategory =
+    nonEmptyCategories[Math.floor(Math.random() * nonEmptyCategories.length)];
+  updateQuestionForCategory(selectedCategory);
+}
+
+function handleSpecificCategory(category) {
+  selectedCategory = category;
+  const categoryQuestions = allQuestions[selectedCategory];
+
+  if (categoryQuestions.length === 0) {
+    disableCategoryButton(selectedCategory);
+    return;
+  }
+
+  updateQuestionForCategory(selectedCategory);
+}
+
+function updateQuestionForCategory(category) {
+  const card = document.querySelector(".card");
+  const front = card.querySelector(".front");
+  const back = card.querySelector(".back");
+
+  const categoryQuestions = allQuestions[category];
+  const randomIndex = Math.floor(Math.random() * categoryQuestions.length);
+  const randomQuestion = categoryQuestions[randomIndex];
+
+  categoryQuestions.splice(randomIndex, 1);
+  displayedCards.push(randomQuestion);
+
+  front.innerText = randomQuestion.question;
+  back.innerText = randomQuestion.answer;
+
+  card.classList.remove(
+    "category1",
+    "category2",
+    "category3",
+    "category4",
+    "category5"
+  );
+  card.classList.add("category" + category);
+  currentQuestion = randomQuestion;
+}
+
+function disableAllCategoryButtons() {
+  document.querySelectorAll(".categories button").forEach((button) => {
+    button.classList.add("disabled");
+  });
+}
+
+function disableCategoryButton(category) {
+  let categoryButton = document.querySelector(
+    `.categories button[data-category="${category}"]`
+  );
+  categoryButton.classList.add("disabled");
+}
+
+function disableRandomCategoryButton() {
+  let randomCategoryButton = document.querySelector('[data-category="random"]');
+  randomCategoryButton.classList.remove("random-category");
+  randomCategoryButton.classList.add("disabled");
+}
+
+function updateButtons() {
+  console.log(displayedCards, "displayedCards");
+
+  if (displayedCards.length === 0) {
+    document.querySelector(".button.previous").classList.add("disabled");
+  } else {
+    document.querySelector(".button.previous").classList.remove("disabled");
+    document.querySelector(".button.previous").classList.add("buttonActive");
+  }
+}
+
+function animateContainer() {
+  const container = document.querySelector(".container");
   setTimeout(() => {
     container.classList.remove("zoom-out");
     container.classList.add("zoom-in");
@@ -110,60 +162,52 @@ function DisplayCard(category) {
 }
 
 function displayPreviousQuestion() {
-  if (currentCardIndex === null || currentCardIndex <= 0) return;
-  currentCardIndex--;
-  const previousCard = displayedCards[currentCardIndex];
-  const category = previousCard.category;
-  const questionIndex = previousCard.questionIndex;
-  const container = document.querySelector(".container");
-  container.classList.remove("zoom-in");
-  container.classList.add("zoom-out");
-
-  setTimeout(() => {
-    const card = document.querySelector(".card");
-    const front = card.querySelector(".front");
-    const back = card.querySelector(".back");
-
-    const categoryQuestions = categorizedQuestions[category];
-    const question = categoryQuestions[questionIndex];
-
-    front.innerText = question.question;
-    back.innerText = question.answer;
-
-    card.classList.remove(
-      "category1",
-      "category2",
-      "category3",
-      "category4",
-      "category5"
-    );
-
-    card.classList.add("category" + category);
-
-    // Update current card index
-    currentCardIndex--;
-
-    // Update visibility of previous button
-    if (currentCardIndex === 0) {
-      document.querySelector(".button.previous").classList.add("hidden");
-    } else {
-      document.querySelector(".button.previous").classList.remove("hidden");
-    }
-  }, 300);
-
-  setTimeout(() => {
-    container.classList.remove("zoom-out");
-    container.classList.add("zoom-in");
-  }, 300);
+  //   if (currentCardIndex === null || currentCardIndex <= 0) return;
+  //   currentCardIndex--;
+  //   const previousCard = displayedCards[currentCardIndex];
+  //   const category = previousCard.category;
+  //   const questionIndex = previousCard.questionIndex;
+  //   const container = document.querySelector(".container");
+  //   container.classList.remove("zoom-in");
+  //   container.classList.add("zoom-out");
+  //   setTimeout(() => {
+  //     const card = document.querySelector(".card");
+  //     const front = card.querySelector(".front");
+  //     const back = card.querySelector(".back");
+  //     const categoryQuestions = allQuestions[category];
+  //     const question = categoryQuestions[questionIndex];
+  //     front.innerText = question.question;
+  //     back.innerText = question.answer;
+  //     card.classList.remove(
+  //       "category1",
+  //       "category2",
+  //       "category3",
+  //       "category4",
+  //       "category5"
+  //     );
+  //     card.classList.add("category" + category);
+  //     // Update current card index
+  //     currentCardIndex--;
+  //     // Update visibility of previous button
+  //     if (currentCardIndex === 0) {
+  //       document.querySelector(".button.previous").classList.add("disabled");
+  //     } else {
+  //       document.querySelector(".button.previous").classList.remove("disabled");
+  //     }
+  //   }, 300);
+  //   setTimeout(() => {
+  //     container.classList.remove("zoom-out");
+  //     container.classList.add("zoom-in");
+  //   }, 300);
 }
 
 function displayNextQuestion() {
-  if (
-    currentCardIndex === null ||
-    currentCardIndex >= displayedCards.length - 1
-  )
-    return;
-  currentCardIndex++;
-  const nextCard = displayedCards[currentCardIndex];
-  DisplayCard(nextCard.category);
+  //   if (
+  //     currentCardIndex === null ||
+  //     currentCardIndex >= displayedCards.length - 1
+  //   )
+  //     return;
+  //   currentCardIndex++;
+  //   const nextCard = displayedCards[currentCardIndex];
+  //   DisplayCard(nextCard.category);
 }
